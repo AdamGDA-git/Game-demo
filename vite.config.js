fdbc28eedbd3c7fe09614373e6ba098a6d5f4711
AdamGDA-git <adamgamer11119999@gmail.com>
2025-09-09 21:50:21 +0300

    actually renaming public because the fucking host uses linux

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 
 
-  publicDir: path.resolve(__dirname, 'Public'),
+  publicDir: path.resolve(__dirname, 'public'),
   
   envDir: '../',
   server: {
