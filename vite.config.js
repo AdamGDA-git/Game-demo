c7c418b6052d6e9895f0ebe3f280f983d4149dbd
AdamGDA-git <adamgamer11119999@gmail.com>
2025-09-09 21:35:29 +0300

    the fucking public directory is lowercase , this shit is fucking case sensitive , i have been doing shit for 2 days and this was the problem

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+
+  publicDir: path.resolve(__dirname, 'public'),
+  
   envDir: '../',
   server: {
     proxy: {
