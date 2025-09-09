2850189f749f14e5d60cf1ee59fcf0b3c140ea98
AdamGDA-git <adamgamer11119999@gmail.com>
2025-09-09 21:59:16 +0300

    removing the script that tells the thing that the capitalized public folder is the public one

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 
-
-  publicDir: path.resolve(__dirname, 'public'),
   
   envDir: '../',
   server: {
